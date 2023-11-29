946cc3355115b7717cb2f5aa3ccba96b310e1267
thtech1 <roquelima182@gmail.com>
2023-11-28 22:50:23 -0300

    questao 3

---
+++ questao 3.sql
@@ -0,0 +1,16 @@
+DELIMITER //
+
+CREATE PROCEDURE ListarPasteisComBaconEQueijo()
+BEGIN
+    SELECT p.nome AS nome_pastel
+    FROM produtos p
+    JOIN ingredientes_de_produtos ip_bacon ON p.idproduto = ip_bacon.id_produto
+    JOIN ingredientes i_bacon ON ip_bacon.id_ingrediente = i_bacon.id_ingrediente
+    JOIN ingredientes_de_produtos ip_queijo ON p.idproduto = ip_queijo.id_produto
+    JOIN ingredientes i_queijo ON ip_queijo.id_ingrediente = i_queijo.id_ingrediente
+    WHERE i_bacon.nome = 'bacon'
+        AND i_queijo.nome = 'queijo';
+END //
+
+DELIMITER ;
+CALL ListarPasteisComBaconEQueijo();
