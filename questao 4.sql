4a5a9b9ef3e2644e3bcf75dbece4275fb84987fa
thtech1 <roquelima182@gmail.com>
2023-11-28 22:25:59 -0300

    Correção da qst 4

--- questao 4.sql
+++ questao 4.sql
@@ -1,16 +1,21 @@
+
+DELIMITER //
+DROP PROCEDURE IF EXISTS CalcularValorVendaTotalPastels;
+
 DELIMITER //
 
 CREATE PROCEDURE CalcularValorVendaTotalPastels()
 BEGIN
     DECLARE total_venda DOUBLE;
 
-    SELECT SUM(preco_unitario * quantidade) INTO total_venda
-    FROM itens_do_pedido
-    where id_produto = 1;
+    SELECT SUM(it.preco_unitario * it.quantidade) INTO total_venda
+    FROM itens_do_pedido it
+    JOIN produtos p ON it.id_produto = p.idproduto
+    WHERE p.id_categoria_prod = 1;
 
     SELECT total_venda AS ValorVendaTotal;
 END //
 
 DELIMITER ;
-CALL CalcularValorVendaTotalPastels();
 
+CALL CalcularValorVendaTotalPastels();
