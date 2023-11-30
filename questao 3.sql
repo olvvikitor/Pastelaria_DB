e9d87325776d7d8fae54c37123aacd413e6bbc96
thtech1 <roquelima182@gmail.com>
2023-11-30 01:03:51 -0300

    questões 2,3,4,6,10

--- questao 3.sql
+++ questao 3.sql
@@ -1,7 +1,3 @@
-DELIMITER //
-
-CREATE PROCEDURE ListarPasteisComBaconEQueijo()
-BEGIN
     SELECT p.nome AS nome_pastel
     FROM produtos p
     JOIN ingredientes_de_produtos ip_bacon ON p.idproduto = ip_bacon.id_produto
@@ -10,7 +6,4 @@ BEGIN
     JOIN ingredientes i_queijo ON ip_queijo.id_ingrediente = i_queijo.id_ingrediente
     WHERE i_bacon.nome = 'bacon'
         AND i_queijo.nome = 'queijo';
-END //
-
-DELIMITER ;
-CALL ListarPasteisComBaconEQueijo();
+-- Liste todos os pastéis que possuem bacon e queijo em seu recheio.
--- questao 4.sql
+++ questao 4.sql
@@ -1,21 +1,7 @@
-
-DELIMITER //
-DROP PROCEDURE IF EXISTS CalcularValorVendaTotalPastels;
-
-DELIMITER //
-
-CREATE PROCEDURE CalcularValorVendaTotalPastels()
-BEGIN
-    DECLARE total_venda DOUBLE;
-
-    SELECT SUM(it.preco_unitario * it.quantidade) INTO total_venda
+  SELECT SUM(it.preco_unitario * it.quantidade)
     FROM itens_do_pedido it
     JOIN produtos p ON it.id_produto = p.idproduto
     WHERE p.id_categoria_prod = 1;
 
-    SELECT total_venda AS ValorVendaTotal;
-END //
+-- Mostre o valor de venda total de todos os pastéis cadastrados no sistema.
 
-DELIMITER ;
-
-CALL CalcularValorVendaTotalPastels();
